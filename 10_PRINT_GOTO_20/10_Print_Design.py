b0d9bc4f02b19ec98a1e57024e2d247b65b64818
Mo Afshar <moafshaar@gmail.com>
2019-12-31 12:43:17 +0000

    10 print design

--- 10_PRINT_GOTO_20/10_Print_Design.py
+++ 10_PRINT_GOTO_20/10_Print_Design.py
@@ -1,1 +1,25 @@
 ## 10 Print Design
+from random import random
+
+x, y = 0, 0
+spacing = 10
+
+def setup():
+    size(800, 300)
+    background(0)
+
+def draw():
+    global x
+    global y
+    global spacing
+    stroke(255)
+    if random() < 0.5:
+        line(x, y, x+spacing, y+spacing)
+    else:
+        line(x, y+spacing, x+spacing, y)
+    x = x + 10
+    if x > width:
+        x = 0
+        y = y + 10
+    if y > height:
+        noLoop()
